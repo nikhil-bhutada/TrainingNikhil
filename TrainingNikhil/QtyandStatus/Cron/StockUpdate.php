32e04c7ff631aa99215d56e8128deb9dccf77b4f
Nikhil Bhutada <nikhil.bhutada@krishtechnolabs.com>
2021-05-26 09:54:55 +0530

    Cron File Updated

--- TrainingNikhil/QtyandStatus/Cron/StockUpdate.php
+++ TrainingNikhil/QtyandStatus/Cron/StockUpdate.php
@@ -1,9 +1,45 @@
 <?php
 
 namespace TrainingNikhil\QtyandStatus\Cron;
-
+use \Psr\Log\LoggerInterface;
+use Magento\Framework\File\Csv;
+use Magento\Catalog\Model\Product;
+use Magento\Framework\App\Cache\TypeListInterface;
+use Magento\Framework\App\Cache\Frontend\Pool;
+use Magento\Catalog\Api\ProductRepositoryInterface;
+use Magento\CatalogInventory\Api\StockRegistryInterface;
 class StockUpdate
 {
+	 protected $logger;
+
+  public function __construct(LoggerInterface $logger,
+Csv $csv,
+Product $product,
+TypeListInterface $typelist,
+Pool $pool,
+ProductRepositoryInterface $productrepo,
+StockRegistryInterface $stock
+  ) {
+
+    $this->logger = $logger;
+    $this->csv = $csv;
+    $this->product = $product;
+    $this->typelist = $typelist;
+    $this->pool = $pool;
+    $this->productrepo = $productrepo;
+    $this->stock = $stock;
+
+  }
+
+  /**
+
+    * Write to system.log
+
+    *
+
+    * @return void
+
+  */
 
 	public function execute()
 	{
@@ -11,21 +47,16 @@ class StockUpdate
 		$writer = new \Zend\Log\Writer\Stream(BP . '/var/log/cron.log');
 		$logger = new \Zend\Log\Logger();
 		$logger->addWriter($writer);
-		$logger->info(__METHOD__);
+		$logger->info(_METHOD_);
 		 try {
         /* Some logic that could throw an Exception */
-        	$objectManager = \Magento\Framework\App\ObjectManager::getInstance();
-        	$csvobj = $objectManager->create('Magento\Framework\File\Csv');
-        	$loggerobj = $objectManager->create('Psr\Log\LoggerInterface');
-        	$productobj = $objectManager->create('Magento\Catalog\Model\Product');
-        	$_cacheTypeList = $objectManager->create('Magento\Framework\App\Cache\TypeListInterface');
-    		$_cacheFrontendPool = $objectManager->create('Magento\Framework\App\Cache\Frontend\Pool');   
-        	$file = '';//location from file to be picked
-          	// $DestinationRoot  = '/var/www/html/td.axtrics.com/var/var_import/stock/processed';
-           	// if(!is_dir($DestinationRoot))
-           	// {
-            //   mkdir($DestinationRoot,0777,true);
-            // }
+        	
+        	$file = '/var/www/html/td.axtrics.com/var/var_import/stock/'; // location of file 
+          	$DestinationRoot  = '/var/www/html/td.axtrics.com/var/var_import/stock/processed'; /// location of file after processed
+           	if(!is_dir($DestinationRoot))
+           	{
+              mkdir($DestinationRoot,0777,true);
+            }
 			// get current directory path
 			// set file pattern
 			$file .= "*.csv";
@@ -38,37 +69,35 @@ class StockUpdate
 			});
 			foreach($files as $item){
 			$i = 0;
-			$csv = $csvobj->getData($item);
+			$csv = $this->csv->getData($item);
 		   	foreach ($csv as $row => $data ) 
 		   	{ 
-		   	if($productobj->getIdBySku($data[0]) && is_numeric($data[1])) 
+		   	if($this->product->getIdBySku($data[0]) && is_numeric($data[1])) 
 		   	{
-	          $productRepository = $objectManager->create('Magento\Catalog\Api\ProductRepositoryInterface');
-	          $stockRegistry = $objectManager->create('Magento\CatalogInventory\Api\StockRegistryInterface');
           		//Load product by SKU
-         	  $product = $productRepository->get($data[0]); 
+         	  $product = $this->productrepo->get($data[0]); 
          		//Need to load stock item
-	          $stockItem = $stockRegistry->getStockItem($product->getId());
+	          $stockItem = $this->stock->getStockItem($product->getId());
 	          $stockItem->setData('qty',$data[1]); //set updated quantity
-	          $stockItem->setData('manage_stock',$stockData['manage_stock']);
-	          $stockItem->setData('is_in_stock',$stockData['is_in_stock']);
-	          $stockItem->setData('use_config_notify_stock_qty',1);
-          	  $stockRegistry->updateStockItemBySku($data[0], $stockItem);
+	          //$stockItem->setData('manage_stock',$stockData['manage_stock']);
+	          //$stockItem->setData('is_in_stock',$stockData['is_in_stock']);
+	          //$stockItem->setData('use_config_notify_stock_qty',1);
+          	  $this->stock->updateStockItemBySku($data[0], $stockItem);
 		   		}
        		  $i++;
-       		// if(sizeof($csv)<=$i){
-         //          $file1 = basename($item); 
-         //          $DestinationFile = $DestinationRoot."/".$file1; // Create the destination filename 
-         //          rename($item, $DestinationFile); // rename the file     
-         //          $_cacheTypeList->cleanType('full_page');
-		    	  // $cacheFrontend->getBackend()->clean();
-      			// 	}
+       		if(sizeof($csv)<=$i){
+                  $file1 = basename($item); 
+                  $DestinationFile = $DestinationRoot."/".$file1; // Create the destination filename 
+                  rename($item, $DestinationFile); // rename the file     
+                  $this->typelist->cleanType('full_page');
+		    	  $this->pool->getBackend()->clean();
+      				}
      			}
    			}
  			} catch (\Exception $e) 
  				{
  					$product=false;
-					$loggerobj->critical($e->getMessage());
+					 $this->logger->critical($e->getMessage());
     			}
 				return $this;
 
